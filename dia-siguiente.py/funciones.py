8edc6323ab77f691b5cebb2abd3c9cb7729c0715
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-21 19:32:18 +0100

    clase edificios

--- dia-siguiente.py/funciones.py
+++ dia-siguiente.py/funciones.py
@@ -2,3 +2,11 @@ class Ciudades:
     def __init__(self, NY, LAS):
         self.NY = NY
         self.LAS = LAS
+
+class Edificios(Ciudades):
+    def __init__(self, NY, LAS, A, B, C):
+        super().__init__(NY, LAS)
+        self.A = A
+        self.B = B
+        self.C = C
+
