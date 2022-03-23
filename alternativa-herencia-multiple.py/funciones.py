86264e1f379841e2217d509670ba23902c8ed457
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-23 19:11:39 +0100

    Orientación de las paredes

--- alternativa-herencia-multiple.py/funciones.py
+++ alternativa-herencia-multiple.py/funciones.py
@@ -11,3 +11,11 @@ class Casa(Ventana, Pared):
         Ventana.__init__(self, superficie)
         Pared.__init__(self, orientacion)
 
+#Orientación de las paredes
+p_n = Pared("pared norte")
+p_s = Pared("pared sur")
+p_e = Pared("pared este")
+p_o = Pared("pared oeste")
+
+#Superficies de las ventanas
+
