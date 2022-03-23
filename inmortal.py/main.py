410271bcc4ecce8937c4b7a0356251baeaf0278e
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-23 18:29:25 +0100

    primera respuesta

---
+++ inmortal.py/main.py
@@ -0,0 +1,5 @@
+'''
+Se muestra antes el signo de intrrogación antes que el mensaje, porque solo va a pasar a la función
+yang.__del__() cuando termine el programa o cuando se borren todos los objetos. Pero al haber un print("?")
+justo depués del del(yang) entonces se imprimirá cuando ya haya impreso "?"
+'''
