b987a879fe6033ffdb9ab3d0b8d12da3b80e0b6d
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-23 20:17:36 +0100

    #asociamos a cada pared con una ventana y su superficie

--- alternativa-herencia-multiple.py/funciones.py
+++ alternativa-herencia-multiple.py/funciones.py
@@ -9,17 +9,13 @@ class Pared: #La pared contiene ventanas
         
     def añadir_ventanas(self, v):
         self.ventana.append(v)
+    
+    def devolver(self):
+            return self.ventana
 
 class Ventana:
-    def __init__(self, pared, superficie):
-        self.pared = pared
+    def __init__(self, superficie):
         self.superficie = superficie
-        
-        
-
-
-         
-
 
 #Orientación de las paredes
 p_n = Pared("pared norte")
@@ -28,10 +24,14 @@ p_e = Pared("pared este")
 p_o = Pared("pared oeste")
 
 #Superficies de las ventanas
-v_n = Ventana (p_n, 3)
-v_s = Ventana (p_s, 5)
-v_e = Ventana (p_e, 7)
-v_o = Ventana (p_o, 0.3)
+v_n = Ventana (3)
+v_s = Ventana (5)
+v_e = Ventana (7)
+v_o = Ventana (0.3)
 
-casa = Casa(v_n, p_n )
+#asociamos a cada pared con una ventana y su superficie
+p_n.añadir_ventanas(v_n)
+p_s.añadir_ventanas(v_s)
+p_e.añadir_ventanas(v_e)
+p_o.añadir_ventanas(v_o)
 
