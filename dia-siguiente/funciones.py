6390fba366c6b0ff739729374b11a7a345e01983
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-26 11:55:08 +0100

    modificaciones en las llamadas

--- dia-siguiente/funciones.py
+++ dia-siguiente/funciones.py
@@ -3,16 +3,11 @@ class Ciudades:
         self.nombre = nombre # NY, LA
         self.l_edif = [] #lista donde guardamos los edificios que pertenecen a una ciudad
         
-    def añadir_edificios(self, edificio):
-        self.l_edif.append(edificio)
-        
-    def devolver(self):
-        return self.l_edif
     
 class Edificios:
     def __init__(self, nomEdif, ciudad):
         self.nomEdif = nomEdif # A, B, C
-        self.ciudad = ciudad
+        self.Ciudades = ciudad
         ciudad.Edificios.append(nomEdif) 
 
 
@@ -20,34 +15,35 @@ class Empresa: #me crea los atributos para luego guardármelos fuera
     def __init__(self, nomEmpr):
         self.nomEmpr = nomEmpr # Nombre de la empresa (Yoohoo)
         self.l_empl = [] #lista de los empleados
-        self.l_edificios = [] #lista que contiene todos los edificios de la empresa
+        #self.l_edificios = [] #lista que contiene todos los edificios de la empresa
 
 class Empleados: #clase que nos crea los atributos asociados a los nombres de los empleados
     def __init__(self, nomEmpl, empre):
         self.nomEmpl = nomEmpl
         self.empre = empre
+        empresa.l_empl.append(nomEmpl)
+
+
+#Ciudades
+ciudad1 = Ciudades("NY")
+ciudad2 = Ciudades("Los Ángeles")
 
 #Empresa
 empresa = Empresa("Yoohoo")
        
-#Empleados
+#Empleados de la empresa
 emp1 = Empleados("Martin", empresa)
 emp2 = Empleados("Salim", empresa)
 emp3 = Empleados("Xing", empresa)     
 
 
-#Edificios
+#Edificios de la empresa
 edif1 = Edificios("A", empresa)
 edif2 = Edificios("B", empresa)
 edif3 = Edificios("C", empresa)
 
-
-
-
-#Ciudad
-ciudad1 = Ciudades("NY")
-ciudad2 = Ciudades("Los Ángeles")
-ciudad1.añadir_edificios(edif1) # nos añade a NY A
-ciudad1.añadir_edificios(edif2) # nos añade a NY B
-ciudad2.añadir_edificios(edif3) # nos añade a LA C
+#Edificios en las ciudades
+edif_1 = Edificios("A", ciudad1)
+edif_2 = Edificios("B", ciudad1)
+edif_3 = Edificios("C", ciudad2)
 
